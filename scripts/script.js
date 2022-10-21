ae8c835f297496f55c704ce71c6d26754d0e41da
wholynum <yakushi1000@gmail.com>
2022-10-21 10:24:40 +0700

    add time validation

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ function calcCount() {
 	const hour = document.getElementById('timeCalc').value
 	const population = 591600
 	let count = (population / 10000) * day * hour
-	if (day != 0) {
+	if (day != 0 && hour >= 10 && hour <= 23) {
 		document.getElementById('countCalc').innerHTML = Math.round(count)
 		document.getElementById('infoCalc').style.display = 'block'
 	}
